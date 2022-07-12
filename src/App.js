6242a3aa36d720747ba8b4faa2f8c3e8c9b1e071
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-12 17:04:46 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,60 +1,105 @@
-import "./App.css"
+import { useState } from 'react';
+import './App.css';
 
-function App() { 
+import { Button } from './components/button.component';
+
+const App = () => {
+  const [panelOne, setPanelOne] = useState('0');
+  const [operator, setOperator] = useState('+');
+  const [panelThree, setPanelThree] = useState('0');
+  const [calculatedResult, setCalculatedResult] = useState('0');
+  const [storedResult, setStoredResult] = useState();
+
+  const updatePanelOne = (e) => {
+    if (!(panelOne.includes('.') && e.target.innerText === '.')) {
+      if (panelOne === '0' && e.target.innerText !== '.') {
+        setPanelOne(e.target.innerText);
+      } else setPanelOne(panelOne + e.target.innerText);
+    }
+    if (e.target.innerText === 'Clear') setPanelOne('0');
+  };
+
+  const updateOperator = (e) => setOperator(e.target.innerText);
+
+  const updatePanelThree = (e) => {
+    if (!(panelThree.includes('.') && e.target.innerText === '.')) {
+      if (panelThree === '0' && e.target.innerText !== '.') {
+        setPanelThree(e.target.innerText);
+      } else setPanelThree(panelThree + e.target.innerText);
+    }
+    if (e.target.innerText === 'Clear') setPanelThree('0');
+  };
+
+  const updateCalculatedResult = () => setCalculatedResult(calculateResult());
+
+  const calculateResult = () => {
+    const operators = {
+      '+': Number(panelOne) + Number(panelThree),
+      '-': Number(panelOne) - Number(panelThree),
+      '*': Number(panelOne) * Number(panelThree),
+      '÷': Number(panelOne) / Number(panelThree),
+    };
+
+    if (isNaN(operators[operator])) return 'Cannot divide 0 by 0';
+    if (operators[operator] === Infinity) return 'Cannot divide by 0';
+
+    return operators[operator];
+  };
+
+  const updateStoredResult = () => setStoredResult(calculatedResult);
+  const recallPanelOne = () => setPanelOne(storedResult || '0');
+  const recallPanelThree = () => setPanelThree(storedResult || '0');
+
+  const panelButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.', 'Clear'];
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{panelOne}</p>
+        <div className="numbers">
+          {panelButtons.map((button) => (
+            <Button
+              key={button + 'panelOne'}
+              click={updatePanelOne}
+              inner={button}
+            />
+          ))}
+          <Button click={recallPanelOne} inner="Recall" />
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{operator}</p>
+        <div className="numbers">
+          <Button click={updateOperator} inner="+" />
+          <Button click={updateOperator} inner="-" />
+          <Button click={updateOperator} inner="*" />
+          <Button click={updateOperator} inner="÷" />
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{panelThree}</p>
+        <div className="numbers">
+          {panelButtons.map((button) => (
+            <Button
+              key={button + 'panelThree'}
+              click={updatePanelThree}
+              inner={button}
+            />
+          ))}
+          <Button click={recallPanelThree} inner="Recall" />
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{calculatedResult}</p>
+        <div>
+          <Button click={updateCalculatedResult} inner="=" />
+          <Button click={updateStoredResult} inner="Store" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/button.component.jsx
@@ -0,0 +1,3 @@
+export const Button = ({ click, inner }) => {
+  return <button onClick={click}>{inner}</button>;
+};
